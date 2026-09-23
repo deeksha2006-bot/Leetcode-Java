d46ab61b9bb31792973b34225ac9cc149325fe3a
DEEKSHA H P <dee232006@gmail.com>
2026-09-23 21:52:50 +0530

    Fix formatting of while loop in reverse method

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -2,7 +2,8 @@ class Solution {
     public int reverse(int x) {
         int rev = 0;
 
-        while (x != 0) {
+        while (x != 0) 
+        {
             int digit = x % 10;
             x = x / 10;
 
